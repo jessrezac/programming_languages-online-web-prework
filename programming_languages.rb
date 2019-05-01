b462d7789c78ac4f0721d75ad198d78f4229ed21
Jess Rezac <jess.rezac@gmail.com>
2019-05-01 04:14:08 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,59 @@
+require 'pry'
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+
+  languages.each do |style, languages_by_style|
+    languages_by_style.each do |language, details|
+      new_hash[language] = details
+      new_hash[language][:style] = []
+
+      while style == :oo
+        new_hash[language][:style] << :oo
+      end
+
+      while style == :functional
+        new_hash[language][:style] << :functional
+      end
+    end
+  end
+    binding.pry
+
+  new_hash
+
 end
+
+reformat_languages(languages)
