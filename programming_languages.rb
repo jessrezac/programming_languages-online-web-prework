5565f91f1ba7ed50d3d51f9f9c926745dde41e7f
Jess Rezac <jess.rezac@gmail.com>
2019-05-01 04:22:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -39,7 +39,7 @@ def reformat_languages(languages)
   languages.each do |style, languages_by_style|
     languages_by_style.each do |language, details|
       new_hash[language] = details
-      new_hash[language][:style] << style
+      new_hash[language][:style] << [style]
     end
   end
     binding.pry
