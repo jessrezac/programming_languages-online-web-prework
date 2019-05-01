128ae786569eeaa9ba9b9652e75703f0dc709d6d
Jess Rezac <jess.rezac@gmail.com>
2019-05-01 04:36:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,37 +1,3 @@
-require 'pry'
-
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
-
-  }
-}
-
 def reformat_languages(languages)
   # your code here
   new_hash = {}
@@ -48,11 +14,6 @@ def reformat_languages(languages)
     end
   end
 
-  binding.pry
-
-
   new_hash
 
 end
-
-reformat_languages(languages)
